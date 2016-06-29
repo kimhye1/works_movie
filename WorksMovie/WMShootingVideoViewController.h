f9ece396e4e80cc3aeac03303b9700a41508db41
Naver <Naver@Naverui-iMac.local>
2016-06-29 19:00:17 +0900

    WMShootingViewController 파일 생성

---
+++ WorksMovie/WMShootingVideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  WMShootingVideoViewController.h
+//  WorksMovie
+//
+//  Created by Naver on 2016. 6. 29..
+//  Copyright © 2016년 worksmobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMShootingVideoViewController : UIViewController
+
+@end
