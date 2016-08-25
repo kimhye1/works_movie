e24c017f1ba8afa79f0e5cd86b97dbe9c91185a7
kimhye1 <hye1.kim@worksmobile.com>
2016-08-25 13:46:44 +0900

    commit1

--- WorksMovie/WMAudioSaver.m
+++ WorksMovie/WMAudioSaver.m
@@ -32,12 +32,14 @@
 - (AVMutableComposition *)mergeAudio:(NSURL *)audioURL withVideo:(NSURL *)videoURL audioAvailable:(BOOL)audioAvailable {
     self.composition = [AVMutableComposition composition];
     
+    // audio
     AVURLAsset *audioAsset = [[AVURLAsset alloc]initWithURL:audioURL options:nil];
     CMTimeRange audioTimeRange = CMTimeRangeMake(kCMTimeZero, audioAsset.duration);
     
     AVMutableCompositionTrack *audioCompositionAudioTrack = [self.composition addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
     [audioCompositionAudioTrack insertTimeRange:audioTimeRange ofTrack:[[audioAsset tracksWithMediaType:AVMediaTypeAudio] firstObject] atTime:kCMTimeZero error:nil];
     
+    // video
     AVURLAsset *videoAsset = [[AVURLAsset alloc]initWithURL:videoURL options:nil];
     CMTimeRange videoTimeRange = CMTimeRangeMake(kCMTimeZero, videoAsset.duration);
     
@@ -48,15 +50,31 @@
     
     [videoCompositionVideoTrack insertTimeRange:videoTimeRange ofTrack:[[videoAsset tracksWithMediaType:AVMediaTypeVideo] firstObject] atTime:kCMTimeZero error:nil];
     
+    // video's audio
      AVMutableCompositionTrack *videoCompositionAuidoTrack = [self.composition addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
     
-    if (audioAvailable) {
+    if (audioAvailable) { // 비디오 사운드 그대로 놔둘 때
         [videoCompositionAuidoTrack insertTimeRange:videoTimeRange ofTrack:[[videoAsset tracksWithMediaType:AVMediaTypeAudio] firstObject] atTime:kCMTimeZero error:nil];
     }
     
-    if (!audioAvailable) {
-        AVMutableAudioMixInputParameters *trackMix =[AVMutableAudioMixInputParameters audioMixInputParametersWithTrack:videoCompositionAuidoTrack];
-        [trackMix setVolume:0 atTime:kCMTimeZero];
+    if (!audioAvailable) { // 비디오의 사운드 없앨 때. 후시 녹음 오디오 끝나면 비디오 사운드 다시 나와야함
+//        [videoCompositionAuidoTrack insertTimeRange:audioTimeRange ofTrack:[[videoAsset tracksWithMediaType:AVMediaTypeAudio] firstObject] atTime:kCMTimeZero error:nil];
+//         [videoCompositionAuidoTrack insertTimeRange:videoTimeRange ofTrack:[[videoAsset tracksWithMediaType:AVMediaTypeAudio] firstObject] atTime:kCMTimeZero error:nil];
+        videoCompositionAuidoTrack.preferredVolume = 0;
+        
+        
+//        AVMutableAudioMixInputParameters *trackMix =[AVMutableAudioMixInputParameters audioMixInputParametersWithTrack:videoCompositionAuidoTrack];
+//        [trackMix setVolume:0.0 atTime:kCMTimeZero];
+//        [trackMix setVolumeRampFromStartVolume:0.0 toEndVolume:0.0 timeRange:CMTimeRangeFromTimeToTime(kCMTimeZero, CMTimeRangeGetEnd(audioTimeRange))];
+//        
+////        [videoCompositionAuidoTrack insertTimeRange:videoTimeRange ofTrack:[[videoAsset tracksWithMediaType:AVMediaTypeAudio] firstObject] atTime:CMTimeRangeGetEnd(audioTimeRange) error:nil];
+//        
+//        AVMutableAudioMix *audioMix = [AVMutableAudioMix audioMix];
+//        audioMix.inputParameters = [NSArray arrayWithObject:trackMix];
+        
+//        CMTimeRange fadeInTimeRange = CMTimeRangeFromTimeToTime(time1Added, time2Added);
+//        [audioInputParams1 setVolumeRampFromStartVolume:0 toEndVolume:1 timeRange:fadeInTimeRange];
+//        [self applyAudioMix];
     }
     
     return self.composition;
